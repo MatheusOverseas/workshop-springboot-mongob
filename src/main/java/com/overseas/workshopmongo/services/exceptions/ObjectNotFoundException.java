c0f4dd1677b46927067056bbdd194b9c03a19012
Matheus Cunha <matheus.overseas@gmail.com>
2022-11-23 09:04:32 -0300

    Getting User by Id

---
+++ src/main/java/com/overseas/workshopmongo/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.overseas.workshopmongo.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+}
