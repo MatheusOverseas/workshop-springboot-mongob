8d10967bf5dd73d08a44d366324b8635d671c09e
Matheus Cunha <matheus.overseas@gmail.com>
2022-11-23 09:04:16 -0300

    Getting User by Id

--- src/main/java/com/overseas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/overseas/workshopmongo/resources/UserResource.java
@@ -1,16 +1,16 @@
 package com.overseas.workshopmongo.resources;
 
+import com.overseas.workshopmongo.dto.UserDTO;
 import com.overseas.workshopmongo.entities.User;
 import com.overseas.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -21,8 +21,16 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> listAll(){
+    public ResponseEntity<List<UserDTO>> listAll(){
         List<User> list = userService.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(list);
+        List<UserDTO> listUDTO = list.stream().map(UserDTO::new).toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(listUDTO);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User u = userService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new UserDTO(u));
     }
 }
