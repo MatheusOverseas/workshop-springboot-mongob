0a0c7e3a72d1ebd7c9825ca723705373b8e47eaa
Matheus Cunha <matheus.overseas@gmail.com>
2022-11-25 12:05:36 -0300

    Using DTO to project Author data

--- src/main/java/com/overseas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/overseas/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.overseas.workshopmongo.config;
 
+import com.overseas.workshopmongo.dto.AuthorDTO;
 import com.overseas.workshopmongo.entities.Post;
 import com.overseas.workshopmongo.entities.User;
 import com.overseas.workshopmongo.repositories.PostRepository;
@@ -29,16 +30,16 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.deleteAll();
         postRepository.deleteAll();
-        
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços", maria);
-        Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
-        postRepository.saveAll(Arrays.asList(p1, p2));
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços", new AuthorDTO(maria));
+        Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        postRepository.saveAll(Arrays.asList(p1, p2));
+
 
     }
 }
