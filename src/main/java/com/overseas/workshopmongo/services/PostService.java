4ab9b5c1ac1f5356c02a8b4736198088d53bcccc
Matheus Cunha <matheus.overseas@gmail.com>
2022-11-25 16:30:39 -0300

    Multiple Criteria search with date

--- src/main/java/com/overseas/workshopmongo/services/PostService.java
+++ src/main/java/com/overseas/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.overseas.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,10 @@ public class PostService {
 
     public List<Post> findByTitle(String msg){
         return postRepository.findByTitle(msg);
+    }
+
+    public List<Post> fullSearch(String msg, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+        return postRepository.fullSearch(msg, minDate, maxDate);
     }
 }
